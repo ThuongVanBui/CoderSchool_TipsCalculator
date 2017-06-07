6fd6dfffc04094d3c21e90a54ef8a0fae2beba19
Lon <thuongvanbui39@gmail.com>
2017-06-07 23:29:50 +0700

    Implement Tip Percentage default

--- TipsCalculator/ViewController.swift
+++ TipsCalculator/ViewController.swift
@@ -11,40 +11,50 @@ import UIKit
 var defaults = UserDefaults.standard
 
 class ViewController: UIViewController, UITextFieldDelegate, UIPageViewControllerDelegate{
-//label
+    //label
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
-      @IBOutlet weak var percentTip: UILabel!
+    @IBOutlet weak var percentTip: UILabel!
     @IBOutlet weak var personpayLabel: UILabel!
-//textFill
+    //textFill
     @IBOutlet weak var billText: UITextField!
     @IBOutlet weak var splitLabel: UITextField!
-  //Segment
+    //Segment
     @IBOutlet weak var percentSegment: UISegmentedControl!
-
-   //Button
+    
+    //Button
     @IBOutlet weak var btnMenu: UIBarButtonItem!
     //Slider
     @IBOutlet weak var TipRate: UISlider!
     
     //Variables
     var total = Double()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.billText.delegate = self;
         self.splitLabel.delegate = self;
         // Do any additional setup after loading the view, typically from a nib.
-//       btnMenu.target = revealViewController()
-//        btnMenu.action = #selector(SWRevealViewController.revealToggle(_:))
-      billText.becomeFirstResponder()
+        //       btnMenu.target = revealViewController()
+        //        btnMenu.action = #selector(SWRevealViewController.revealToggle(_:))
+        billText.becomeFirstResponder()
+        if let tipRatePercent = defaults.object(forKey: "TipRate") as? Float {
+            TipRate.value = tipRatePercent
+            percentTip.text = "\(Int(tipRatePercent))%"
+            switch tipRatePercent {
+            case 25: percentSegment.selectedSegmentIndex = 2
+            case 20: percentSegment.selectedSegmentIndex = 1
+            default:
+                percentSegment.selectedSegmentIndex = 0
+            }
+        }
         self.SaveData()
     }
     func SaveData()  {
         defaults.set(billText.text, forKey: "numberOld")
         defaults.set(TipRate.value, forKey: "pct")
         defaults.set(personpayLabel.text, forKey: "personpay")
-
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -53,7 +63,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIPageViewControlle
     }
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         if textField.text == "" && (string == "0" || string == ","){
-        return false
+            return false
         }
         return true
     }
@@ -61,27 +71,30 @@ class ViewController: UIViewController, UITextFieldDelegate, UIPageViewControlle
     @IBAction func Ontap(_ sender: AnyObject) {
         view.endEditing(true)
     }
-   
+    
     @IBAction func item(_ sender: Any) {
         self.SaveData()
-               let printview = self.storyboard?.instantiateViewController(withIdentifier: "PrintViewID") as! PrintBill
+        let printview = self.storyboard?.instantiateViewController(withIdentifier: "PrintViewID") as! PrintBill
         self.navigationController?.pushViewController(printview, animated: true)
     }
     
-   
     
-        @IBAction func calcalatorTips(_ sender: AnyObject) {
-            
+    
+    @IBAction func calcalatorTips(_ sender: AnyObject) {
+        
         let tipPercenttages = [0.18, 0.2, 0.25]
         let bill = Double(billText.text!) ?? 0
         let tip = bill * tipPercenttages[percentSegment.selectedSegmentIndex]
-         total = bill + tip
+        TipRate.value = Float(100 * tipPercenttages[percentSegment.selectedSegmentIndex])
+        defaults.set(TipRate.value, forKey: "TipRate")
+        percentTip.text = "\(Int(100 * tipPercenttages[percentSegment.selectedSegmentIndex]))%"
+        total = bill + tip
         tipLabel.text = String(format:"$%.2f",(tip))
         totalLabel.text = String(format:"$%.2f",(total))
         let split = Double(splitLabel.text!) ?? 1
         let personpay = total / split
         personpayLabel.text = String(format:"$%.2f",(personpay))
-        
+        defaults.synchronize()
     }
     
     @IBAction func btnAqua() {
@@ -100,27 +113,29 @@ class ViewController: UIViewController, UITextFieldDelegate, UIPageViewControlle
     @IBAction func btnSalmon() {
         self.view.backgroundColor = UIColor.init(red: 255/255, green: 102/255, blue: 102/255, alpha: 1)
     }
-        
+    
     @IBAction func TipPercentageValueChange(_ sender: UISlider) {
         let bill1 = (billText.text!as NSString).doubleValue
         let person = Double(splitLabel.text!) ?? 1
         let sliderValue = Int(sender.value)
         let myTipPct = (Double(sliderValue) / 100)
         
+        print(sender.value)
         
-      
-    let tipBill = bill1 * myTipPct
+        let tipBill = bill1 * myTipPct
         let totalBill = bill1 + tipBill
         let psPay = (totalBill/person)
         percentTip.text = "\(Int(sliderValue))%"
         tipLabel.text = String(format:"$%.2f",tipBill)
-totalLabel.text = String(format:"$%.2f",totalBill)
+        totalLabel.text = String(format:"$%.2f",totalBill)
         personpayLabel.text = String(format:"$%.2f",psPay)
+        defaults.set(sender.value, forKey: "TipRate")
         
-       
+        defaults.synchronize()
         
-    }
-   
+        
     }
     
+}
+
 
